3ca9e04b6d4f5e19ba6881a797641aa0ab82261d
kn52 <ashish52922@gmail.com>
2024-03-27 15:52:05 +0530

    d

---
+++ node/test.js
@@ -0,0 +1,13 @@
+import * as calc from './calculator';
+
+console.log('welcome to node js');
+
+
+function sayHello() {
+    console.log('welcome hello');
+}
+console.log(global);
+
+sayHello();
+
+console.log(calc);
